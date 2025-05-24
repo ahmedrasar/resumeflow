7087ce49180618c62500820183885381f20dfe91
Ahmed R. Asar <ahmedragabasr0346@gmail.com>
2025-05-24 14:40:43 +0300

    Refactor: Formatting and Tidding

--- lib/configuration/view_constrains/view_constrains.dart
+++ lib/configuration/view_constrains/view_constrains.dart
@@ -1,15 +1,14 @@
 import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/foundation.dart';
-import 'package:window_manager/window_manager.dart' show windowManager, WindowManager;
-
-const _windowsMinSize = Size(1280, 720);
+import 'package:window_manager/window_manager.dart'
+    show windowManager, WindowManager;
 
 Future<void> configureViewConstrains() async {
   if (kIsWeb) return;
 
   if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
     await windowManager.ensureInitialized();
-    WindowManager.instance.setMinimumSize(_windowsMinSize);
+    WindowManager.instance.setMinimumSize(Size(1280, 720));
   }
 }
